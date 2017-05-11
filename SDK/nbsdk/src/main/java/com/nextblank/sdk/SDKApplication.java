17a946ec39963672ae92de9209de74061428555e
juneyu <yu-junjun@qq.com>
2017-05-11 14:53:33 +0800

    add library LeakCanary

--- SDK/nbsdk/src/main/java/com/nextblank/sdk/SDKApplication.java
+++ SDK/nbsdk/src/main/java/com/nextblank/sdk/SDKApplication.java
@@ -3,6 +3,7 @@ package com.nextblank.sdk;
 import android.app.Application;
 
 import com.nextblank.sdk.tools.Tools;
+import com.squareup.leakcanary.LeakCanary;
 
 /**
  * Application相关初始化工作
@@ -17,6 +18,12 @@ public class SDKApplication extends Application {
         instance = this;
         //Application在此初始化
         Tools.init(this);
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
     }
 
     public static SDKApplication getInstance() {
