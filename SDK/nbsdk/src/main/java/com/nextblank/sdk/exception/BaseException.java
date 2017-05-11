f773156e9b4bb803b2dfd00713f5d4b9cb4125ea
juneyu <yu-junjun@qq.com>
2017-05-11 09:59:58 +0800

    add exception

--- SDK/nbsdk/src/main/java/com/nextblank/sdk/exception/BaseException.java
+++ SDK/nbsdk/src/main/java/com/nextblank/sdk/exception/BaseException.java
@@ -1,13 +1,13 @@
 package com.nextblank.sdk.exception;
 
-public class AppLayerException extends Exception {
+public class BaseException extends Exception {
 
     private static final long serialVersionUID = 1L;
 
     public int code;
     public String message;
 
-    public AppLayerException(Throwable throwable, int code) {
+    public BaseException(Throwable throwable, int code) {
         super(throwable);
         this.code = code;
         this.message = throwable.getMessage();
--- SDK/nbsdk/src/main/java/com/nextblank/sdk/exception/IllegalArguException.java
+++ SDK/nbsdk/src/main/java/com/nextblank/sdk/exception/IllegalArguException.java
@@ -1,12 +1,8 @@
 package com.nextblank.sdk.exception;
 
-public class IllegalArgumentException extends BaseException {
+public class IllegalArguException extends BaseException {
 
-    public IllegalArgumentException(Throwable throwable, int code) {
+    public IllegalArguException(Throwable throwable, int code) {
         super(throwable,code);
-    }
-
-    public void IllegalArgumentException(String msg, Object... params) {
-        throw new java.lang.IllegalArgumentException(String.format(msg, params));
     }
 }
