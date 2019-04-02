280d296aa87383ce23892d8f72e0bea214d731b7
juneyu <yu-junjun@qq.com>
2019-04-02 17:41:21 +0800

    CircleRectFooter

--- SDK/portal/src/main/java/com/pn/android/portal/MainActivity.java
+++ SDK/portal/src/main/java/com/pn/android/portal/MainActivity.java
@@ -5,19 +5,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.pn.android.portal.ui.MagicCircle;
+import com.pn.android.portal.ui.CircleRectFooter;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btnstart;
-    private MagicCircle circle3;
+    private CircleRectFooter circle3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        circle3 = (MagicCircle)findViewById(R.id.circle3);
+        circle3 = (CircleRectFooter)findViewById(R.id.circle3);
         this.btnstart = (Button) findViewById(R.id.btn_start);
         btnstart.setOnClickListener(new View.OnClickListener() {
             @Override
